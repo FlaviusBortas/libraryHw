83fcff47627b1e71847cb9b01db52590fa5bdc3b
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-09-05 21:01:23 -0400

    finished

--- libraryHw/AddGameTableViewController.swift
+++ libraryHw/AddGameTableViewController.swift
@@ -8,15 +8,58 @@
 
 import UIKit
 
+protocol AddGameDelegate: AnyObject {
+    
+    func addGame(game: Game)
+}
+
 class AddGameTableViewController: UITableViewController {
+    
+    @IBOutlet weak var gameTextField: UITextField!
+    @IBOutlet weak var studioTextField: UITextField!
+    
+    weak var delegate: AddGameDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        gameTextField.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func doneTapped(_ sender: UIBarButtonItem) {
+        guard let gameText = gameTextField.text, gameText.isNotEmpty else {
+            gameTextField.becomeFirstResponder()
+            return
+            
+        }
+          guard let studioText = studioTextField.text, studioText.isNotEmpty else  {
+            studioTextField.becomeFirstResponder()
+            return
+        }
+        
+        let game = Game(name: gameText, studio: studioText)
+        delegate?.addGame(game: game)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
 }
+
+
+extension String {
+    var isNotEmpty: Bool {
+        return !self.isEmpty
+    }
+}
+
+
+
+
+
